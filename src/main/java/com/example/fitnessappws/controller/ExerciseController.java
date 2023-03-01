886f606cf57e399600bdea467f77831723ab6b35
Matyas <matyas.szabo@protonmail.com>
2023-03-01 11:07:10 +0100

    create controllers

---
+++ src/main/java/com/example/fitnessappws/controller/ExerciseController.java
@@ -0,0 +1,40 @@
+package com.example.fitnessappws.controller;
+
+import com.example.fitnessappws.dao.ExerciseDAO;
+import com.example.fitnessappws.model.Exercise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/exercises")
+public class ExerciseController {
+    private final ExerciseDAO exerciseDAO;
+
+    @Autowired
+    public ExerciseController(ExerciseDAO exerciseDAO) {
+        this.exerciseDAO = exerciseDAO;
+    }
+
+    @GetMapping
+    public List<Exercise> getAllExercises() {
+        return exerciseDAO.getAllExercises();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Exercise> getExerciseById(@PathVariable("id") int id) {
+        return exerciseDAO.getExercise(id);
+    }
+
+    @PostMapping
+    public void addExercise(@RequestBody Exercise exercise) {
+        exerciseDAO.addExercise(exercise);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteExercise(@PathVariable("id") int id) {
+        exerciseDAO.deleteExercise(id);
+    }
+}
---
+++ src/main/java/com/example/fitnessappws/controller/WorkoutController.java
@@ -0,0 +1,46 @@
+package com.example.fitnessappws.controller;
+
+import com.example.fitnessappws.dao.WorkoutDAO;
+import com.example.fitnessappws.model.Workout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/workouts")
+public class WorkoutController {
+    private final WorkoutDAO workoutDAO;
+
+    @Autowired
+    public WorkoutController(WorkoutDAO workoutDAO) {
+        this.workoutDAO = workoutDAO;
+    }
+
+    @GetMapping
+    public List<Workout> getAllWorkouts() {
+        return workoutDAO.getAllWorkouts();
+    }
+
+    @GetMapping
+    public List<Workout> listWorkoutsByName(@RequestParam("q") String name) {
+        return workoutDAO.listWorkoutsByName(name);
+    }
+
+
+    @GetMapping("/{id}")
+    public Optional<Workout> getWorkoutById(@PathVariable("id") int id) {
+        return workoutDAO.getWorkout(id);
+    }
+
+    @PostMapping
+    public void addWorkout(@RequestBody Workout workout) {
+        workoutDAO.addWorkout(workout);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteWorkout(@PathVariable("id") int id) {
+        workoutDAO.deleteWorkout(id);
+    }
+}
